ba0c7b5b83f5de74b2cf872f9a3f1758e1f2129e
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2023-11-22 17:27:05 -0300

    função login feita

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,5 +1,8 @@
 const knex = require('../database/conection')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const senhasegura = 'umaSenhaSegura'
 
 const regiterUser = async (req, res) => {
     const { name, email, password } = req.body;
@@ -31,6 +34,31 @@ const regiterUser = async (req, res) => {
 
 }
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const user = await knex('users').where({ email }).first();
 
-module.exports = regiterUser
+        if (!user) {
+            return res.status(404).json({ mensagem: "Usuário não encontrado" });
+        }
+
+        const validPassword = await bcrypt.compare(password, user.user_password);
+
+        if (!validPassword) {
+            res.status(400).json({ mensagem: "Senha inválida" });
+        }
+
+        const token = jwt.sign({ id: user.id }, senhasegura, { expiresIn: '8h' });
+
+        const { user_password, ...userLogged } = user;
+
+        return res.status(200).json({ ...userLogged, token });
+
+    } catch (error) {
+        return res.status(500).json({ mensagem: "Erro interno do servidor" });
+    }
+}
+
+module.exports = { regiterUser, loginUser };
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const regiterUser = require('./controllers/usuarios')
+const { regiterUser, loginUser } = require('./controllers/usuarios')
 
 
 const routes = express()
 
-routes.post('/sign-up', regiterUser)
-
+routes.post('/sign-up', regiterUser);
+routes.post('/sign-in', loginUser)
 
 
 module.exports = routes
