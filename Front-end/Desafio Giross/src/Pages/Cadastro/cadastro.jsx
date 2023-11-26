35999c13d604843cb52aed28dcc238272f8ae8bc
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2023-11-25 21:25:08 -0300

    pagina login e cadastro iniciada

--- Front-end/Desafio Giross/src/Pages/Cadastro/cadastro.jsx
+++ Front-end/Desafio Giross/src/Pages/Cadastro/cadastro.jsx
@@ -1,8 +1,99 @@
 import "./Style.css"
+import Imput from '../../components/Input/Input'
+import Botao from '../../components/Botao/Botao'
+import api from '../../service/instancia'
+import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+
 
 export default function Cadastro() {
 
+    const navigate = useNavigate();
+
+    const [errorUsuario, setErrorUsuario] = useState('')
+
+    const [form, setForm] = useState({
+        nome: '',
+        email: '',
+        senha: '',
+    })
+
+    const [confrmarSenha, setConfirmarSenha] = useState('')
+
+    function input(e) {
+        const chave = e.target.name;
+        const valor = e.target.value;
+        setForm({ ...form, [chave]: valor })
+    }
+
+    const submit = async (e) => {
+        e.preventDefault();
+
+        if (form.senha !== confrmarSenha) {
+            setErrorUsuario('senha e confirmar senha estao diferente')
+            return
+        }
+
+        try {
+            await api.post('/usuario', form);
+            navigate("/");
+        } catch (error) {
+            setErrorUsuario(error.response.data.mensagem)
+        }
+    }
+
     return (
-        <h1>Ola Mundo cadastro</h1>
+        <>
+            <div className='background'>
+
+                <div className='cadastro-tela'>
+                    <div className='cadastro-formulario'>
+
+                        <h1 className='cadastro-formulario-titulo'>Cadastro</h1>
+
+                        <form onSubmit={submit}>
+                            <Imput
+                                etiqueta='nome'
+                                type='text'
+                                name='nome'
+                                value={form.nome}
+                                onChange={input}
+                            />
+
+                            <Imput
+                                etiqueta='email'
+                                type='text'
+                                name='email'
+                                value={form.email}
+                                onChange={input}
+                            />
+
+                            <Imput
+                                etiqueta='senha'
+                                type='password'
+                                name='senha'
+                                value={form.senha}
+                                onChange={input}
+                            />
+
+                            <Imput
+                                etiqueta='confirmar senha'
+                                type='password'
+                                name='confirmar'
+                                value={confrmarSenha}
+                                onChange={e => setConfirmarSenha(e.target.value)}
+                            />
+
+                            <p className='cadastro-formulario-errosenha' >{errorUsuario}</p>
+
+                            <Botao children='Cadastrar' />
+                        </form>
+
+                        <Link className='cadastro-formulario-link' to={'/'}>Já tem cadastro? Clique aqui!</Link>
+                    </div>
+                </div>
+
+            </div>
+        </>
     )
 }
--- Front-end/Desafio Giross/src/Pages/Login/Login.jsx
+++ Front-end/Desafio Giross/src/Pages/Login/Login.jsx
@@ -1,8 +1,86 @@
+import { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
+import Botao from '../../components/Botao/Botao';
+import Imput from '../../components/Input/Input';
+import api from '../../service/instancia';
 import "./Style.css"
 
+
 export default function Login() {
+    const navigate = useNavigate()
+    const [errorUsuario, setErrorUsuario] = useState('')
+    const [form, setForm] = useState({
+        email: '',
+        senha: ''
+    })
+
+    function input(e) {
+        const chave = e.target.name;
+        const valor = e.target.value;
+        setForm({ ...form, [chave]: valor })
+    }
+
+    async function submit(e) {
+        e.preventDefault();
+        try {
+            const response = await api.post('/login', form);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('usuario', response.data.usuario.nome);
+            navigate("/principal");
+        } catch (error) {
+            setErrorUsuario(error.response.data.mensagem)
+        }
+    }
 
     return (
-        <h1>Ola Mundo Login</h1>
+        <div className='background'>
+
+
+
+            <div className='login-tela'>
+
+                <div className='login-texto-botao'>
+
+                    <h1>
+                        OI
+                    </h1>
+                    <p>
+                        OI
+                    </p>
+                    <Link to={'/cadastro'}>
+                        <Botao children='Cadastre-se' />
+                    </Link>
+                </div>
+
+                < div className='login-formulario'>
+
+                    <h1 className='login-formulario-titulo'>Login</h1>
+
+                    <form onSubmit={submit}>
+                        <Imput
+                            etiqueta='email'
+                            type='text'
+                            name='email'
+                            value={form.email}
+                            onChange={input}
+                        />
+
+                        <Imput
+                            etiqueta='senha'
+                            type='password'
+                            name='senha'
+                            value={form.senha}
+                            onChange={input}
+                        />
+
+                        <p className='erro' >{errorUsuario}</p>
+
+                        <Botao children='Entrar' />
+                    </form>
+                </div >
+            </div>
+
+
+        </div >
     )
 }
